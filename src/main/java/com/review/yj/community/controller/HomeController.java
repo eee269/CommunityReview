768fe0e1ac5ed86d9cdc78812997b99743047ddb
eee269 <skfkd3123@gmail.com>
2021-01-09 22:52:26 +0900

    Index

---
+++ src/main/java/com/review/yj/community/controller/HomeController.java
@@ -0,0 +1,13 @@
+﻿package com.review.yj.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
