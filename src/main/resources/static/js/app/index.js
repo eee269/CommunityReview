c2692bc89209a66d84775a409831b85b28e37d5a
eee269 <skfkd3123@gmail.com>
2021-01-16 16:01:22 +0900

    view

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,31 +1,37 @@
-﻿var main = {
+var main = {
     init: function () {
         var _this = this;
 
         // 버튼 클릭했을 때 일어날 함수 지정
-        // $('btnId').on('click', function
+        $('#btn-signUp').on('click', function () {
+            _this.signUp();
+        });
 
     },
     // save버튼 누르면 db에 저장하기
-    save: function () {
+    signUp: function () {
         var data = {
-            // key: $('id').val(),
+            userid: $('#userid').val(),
+            password: $('#password').val(),
+            username: $('#username').val(),
+            nickname: $('#username').val(),
+            phone: $('#phone').val()
         };
 
         $.ajax({
             type: 'POST',
-            url: '/api/v1/posts',
+            url: '/member/signUp',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function () {
-            alert();
+            alert('회원가입이 완료되었습니다.');
             window.location.href = '/';
         }).fail(function () {
             alert(JSON.stringify(error));
-        })
+        });
     }
-}
+};
 
 
 main.init();
